11c3e3bbea15a30b90d1e76c99e5f312f2eebc35
Helena-herasymiuk <he.lena280299@gmail.com>
2019-06-16 17:57:32 +0300

    Updates

---
+++ precache-manifest.a6c2f960a12a474dc23d5b474c2abe15.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6ad6898d80483bfd701a11b0b1af20",
+    "url": "/calender/index.html"
+  },
+  {
+    "revision": "6c676b2cc89c0196c40b",
+    "url": "/calender/static/css/main.43078b9a.chunk.css"
+  },
+  {
+    "revision": "1744db918d7799a6e6d6",
+    "url": "/calender/static/js/2.164617b5.chunk.js"
+  },
+  {
+    "revision": "6c676b2cc89c0196c40b",
+    "url": "/calender/static/js/main.eb00690e.chunk.js"
+  },
+  {
+    "revision": "b00c23bd4c9635b6c78e",
+    "url": "/calender/static/js/runtime~main.74e99931.js"
+  }
+]);
